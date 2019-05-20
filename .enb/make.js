8049987bc1aee2340ac40487015c92a67ef1a696
Дамико Кори <32449464+DamickoKori@users.noreply.github.com>
2019-05-20 14:03:45 +0500

    Update make.js

--- .enb/make.js
+++ .enb/make.js
@@ -40,6 +40,8 @@ const techs = {
         { path: 'node_modules/bem-components/design/common.blocks', check: false },
         { path: 'node_modules/bem-components/design/desktop.blocks', check: false },
         { path: 'node_modules/bem-font-awesome-icons', check: false },
+        'bem-dk-components/common.blocks',
+        'bem-dk-components/design/common.blocks'
         'common.blocks',
         'desktop.blocks',
         'design/common.blocks',
